c64ecbf0c81eba34246eb346d217ce648987e1da
Gilbert Montague <nerdgilbert@users.noreply.github.com>
2016-05-15 16:42:39 -0400

    changed the mapping from brightness to character

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,38 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 #include <opencv2/opencv.hpp>
 #include <opencv/highgui.h>
 
-unsigned char getCharacterByBrightness( int brightness )
+
+/*
+    "Standard" Character ramp for grey scale, black -> white
+    Credit to: http://paulbourke.net/dataformats/asciiart/
+    "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. "
+    or, smaller but more convincing
+    "
+*/
+
+std::string brightnessRamp( "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,^`'. " );
+std::string smallerBrightnessRamp( " .:-=+*#%@" );
+//std::string smallerBrightnessRamp( "@%#*+=-:. " );
+
+float map( int value, int a1, int a2, int b1, int b2 )
 {
-    if( brightness <= 64 )
-    {
-        std::cout << "One" << std::endl;
-        return '@';  
-    }
-    else if( brightness > 64 and brightness <= 128 )
-    {
-        std::cout << "Two" << std::endl;
-        return '*';
-    }
-    else if( brightness > 128 and brightness <= 192 )
-    {
-        std::cout << "Three" << std::endl;
-        return '-';
-    }
-    else
-    {
-        std::cout << "Zero" << std::endl;
-        return ' ';   
-    }
+    return b1 + ( value - a1 )* ( b2 - b1 )/(a2 -a1);
+}
+
+//Lazy mapping from brightness to char
+unsigned char getCharacterByBrightness( float mappedBrightness )
+{
+    return static_cast<unsigned char>( smallerBrightnessRamp.at( mappedBrightness ) );
 }
 
 
 int main( int argc, char** argv )
 {
+    std::cout << "l: " << smallerBrightnessRamp.length() << std::endl;
     if( argc != 2 )
     {
         std::cout << "Usage: " << std::endl;
@@ -62,7 +64,8 @@ int main( int argc, char** argv )
         {
             unsigned char value = grayImage.at<unsigned char>( i, j );
             int brightness = static_cast<int>( value );
-            auto fileBright = getCharacterByBrightness( brightness );
+            auto newValue = map( brightness, 0, 256, 0, smallerBrightnessRamp.length() );
+            auto fileBright = getCharacterByBrightness( newValue );
             
             file << fileBright;
             //std::cout << "Pixel value: " << static_cast<int>( value ) << std::endl;
